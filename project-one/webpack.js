b24c6c921ab92eff05ab206a415c22185bdd9d6e
Taipan <123_marcel@web.de>
2025-10-22 15:53:35 +0200

    Vipe Coding Session I

---
+++ project-one/webpack.js
@@ -0,0 +1,20 @@
+const path = require('path');
+module.exports = {
+entry: './src/main.ts', // Entry point of your TypeScript file
+module: {
+rules: [
+{
+test: /\.tsx?$/,
+use: 'ts-loader',
+exclude: /node_modules/,
+},
+],
+},
+resolve: {
+extensions: ['.tsx', '.ts', '.js'],
+},
+output: {
+filename: 'bundle.js', // Output file
+path: path.resolve(__dirname, 'dist/js'), // Output directory
+},
+};
