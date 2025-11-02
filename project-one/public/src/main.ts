a05d5b310673d802ebd6ca5ceb3de1e6af5d9ac9
Taipan <123_marcel@web.de>
2025-11-02 13:19:31 +0100

    Additional Nutrition Facts

--- project-one/public/src/main.ts
+++ project-one/public/src/main.ts
@@ -6,6 +6,24 @@ import {Html5QrcodeScanner} from "html5-qrcode";
 
 
 // --- Barcode Scanner Initialisierung ---
+  $('#scanBtn').on('click', () => {
+    $('#reader').html('');
+    const scanner = new Html5QrcodeScanner(
+      "reader",
+      { fps: 10, qrbox: 250, 
+        // formatsToSupport: [ "EAN_13", "CODE_128", "UPC_A" ]
+       },
+      false
+      );
+    const onScanSuccess = (decodedText: string , decodedResult: any) => {
+       $('#barcodeInput').val(decodedText);
+       scanner.clear();
+       //&('#reader').html('');
+       $('#searchBtn').trigger('click'); // Suche automatisch auslösen
+     };
+  scanner.render(onScanSuccess, undefined); // todo: onScanError zukunftsproblem
+});
+
 // --- Event Listener für den Search-Button ---
 $('#searchBtn').on('click', async () => {
   const barcode = $('#barcodeInput').val() as string;
@@ -27,17 +45,24 @@ $('#searchBtn').on('click', async () => {
       $('#result').html(`<div class="alert alert-warning">Product not found.</div>`);
       return;
     }
-
+    const ingredients = product.ingredients_hierarchy || [];
+    const topIngredients = ingredients.slice(0, 3);
+    const readableIngredients = topIngredients
+        .map((i: string) => i.replace('en:', '').replace('de:', '')) // Sprachpräfixe entfernen
+        .join(', ');
     // Produktinformationen zusammenstellen
     const html = `
       <div class="card shadow-sm">
         <img src="${product.image_front_small_url || ''}" class="card-img-top" alt="${product.product_name}" />
         <div class="card-body">
           <h5 class="card-title">${product.product_name || 'Unknown Product'}</h5>
-          <p class="card-text">Calories: ${product.nutriments['energy-kcal'] || 'n/a'} kcal</p>
+          <p>Nutriscore: ${(product.nutriscore_grade ?? '').toString().toUpperCase() || 'N/A'}  </p>
+          <p>Ecocore: ${(product.ecoscore_grade ?? '').toString().toUpperCase() || 'N/A'}  </p>
+          <p>Calories: ${product.nutriments['energy-kcal'] || 'n/a'} kcal</p>
           <p>Proteins: ${product.nutriments.proteins || 'n/a'} g</p>
           <p>Carbs: ${product.nutriments.carbohydrates || 'n/a'} g</p>
           <p>Fats: ${product.nutriments.fat || 'n/a'} g</p>
+          <p><strong>Main Ingredients:</strong> ${readableIngredients}</p>
         </div>
       </div>
     `;
