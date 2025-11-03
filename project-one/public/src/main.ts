e2330dff03eb672163146d234f160e403039985e
Taipan <123_marcel@web.de>
2025-11-03 14:55:38 +0100

    Standardisation of filler text in the event of missing data

--- project-one/public/src/main.ts
+++ project-one/public/src/main.ts
@@ -11,17 +11,15 @@ import {Html5QrcodeScanner} from "html5-qrcode";
     const scanner = new Html5QrcodeScanner(
       "reader",
       { fps: 10, qrbox: 250, 
-        // formatsToSupport: [ "EAN_13", "CODE_128", "UPC_A" ]
        },
       false
       );
     const onScanSuccess = (decodedText: string , decodedResult: any) => {
        $('#barcodeInput').val(decodedText);
        scanner.clear();
-       //&('#reader').html('');
        $('#searchBtn').trigger('click'); // Suche automatisch auslösen
      };
-  scanner.render(onScanSuccess, undefined); // todo: onScanError zukunftsproblem
+  scanner.render(onScanSuccess, undefined); // todo: onScanError Zukunftsproblem
 });
 
 // --- Event Listener für den Search-Button ---
@@ -41,10 +39,11 @@ $('#searchBtn').on('click', async () => {
     const response = await $.get(apiUrl);
     const product = response.product;
 
-    if (!product) {
+    if (!product) { 
       $('#result').html(`<div class="alert alert-warning">Product not found.</div>`);
       return;
     }
+    //Zutaten extrahieren und lesbar machen
     const ingredients = product.ingredients_hierarchy || [];
     const topIngredients = ingredients.slice(0, 3);
     const readableIngredients = topIngredients
@@ -58,10 +57,10 @@ $('#searchBtn').on('click', async () => {
           <h5 class="card-title">${product.product_name || 'Unknown Product'}</h5>
           <p>Nutriscore: ${(product.nutriscore_grade ?? '').toString().toUpperCase() || 'N/A'}  </p>
           <p>Ecocore: ${(product.ecoscore_grade ?? '').toString().toUpperCase() || 'N/A'}  </p>
-          <p>Calories: ${product.nutriments['energy-kcal'] || 'n/a'} kcal</p>
-          <p>Proteins: ${product.nutriments.proteins || 'n/a'} g</p>
-          <p>Carbs: ${product.nutriments.carbohydrates || 'n/a'} g</p>
-          <p>Fats: ${product.nutriments.fat || 'n/a'} g</p>
+          <p>Calories: ${product.nutriments['energy-kcal'] || 'N/A'} kcal</p>
+          <p>Proteins: ${product.nutriments.proteins || 'N/A'} g</p>
+          <p>Carbs: ${product.nutriments.carbohydrates || 'N/A'} g</p>
+          <p>Fats: ${product.nutriments.fat || 'N/A'} g</p>
           <p><strong>Main Ingredients:</strong> ${readableIngredients}</p>
         </div>
       </div>
