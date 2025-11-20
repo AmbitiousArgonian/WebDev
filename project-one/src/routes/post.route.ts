41357393e2a6cb6d47152ee28b13d1b206748a8a
Taipan <123_marcel@web.de>
2025-11-20 11:31:48 +0100

    server.ts inital setup

--- project-one/src/routes/post.route.ts
+++ project-one/src/routes/post.route.ts
@@ -1,4 +1,4 @@
-import PostController from "../controllers/post.controller";
+import createBlogPost from "../controllers/post.controller";
 const router = express.Router();
 router.post("/", PostController.createBlogPost);
 export default router;
--- project-one/src/server.ts
+++ project-one/src/server.ts
@@ -1,2 +1,15 @@
 import PostRouter from "./routes/post.route";
+import express from 'express';
+// Create an Express application
+const app = express();
+// Set the port number for the server
+const port = 8000;
+// Define a route for the root path ('/')
+app.get('/', (req, res) => {
+// Send a response to the client
+res.send('Hello, TypeScript + Node.js + Express!');
+});
+
+
+
 app.use("/api/post", PostRouter);
