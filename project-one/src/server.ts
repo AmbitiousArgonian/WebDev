a6d05f50f9095870790265d56aa51f971c96feaa
Taipan <123_marcel@web.de>
2025-11-27 10:33:27 +0100

    server.ts erweiterung von dev.to

--- project-one/src/server.ts
+++ project-one/src/server.ts
@@ -1,8 +1,8 @@
 import PostRouter from "./routes/post.route";
-//import { PrismaClient } from "@prisma/client"; // from essenzial read dev.to
+import { PrismaClient } from "@prisma/client"; // from essenzial read dev.to
 import express from 'express';
 
-//export const prisma = new PrismaClient(); //  from essenzial read dev.to
+export const prisma = new PrismaClient(); //  from essenzial read dev.to
 // Create an Express application
 const app = express();
 
@@ -13,7 +13,30 @@ app.get('/', (req, res) => {
 // Send a response to the client
 res.send('Hello, TypeScript + Node.js + Express!');
 });
+async function main() {
+  app.use(express.json());
 
+  // Register API routes
+  app.use("/api/v1/post", PostRouter);
+
+  // Catch unregistered routes
+  app.all("*", (req: Request, res: Response) => {
+    res.status(404).json({ error: `Route ${req.originalUrl} not found` });
+  });
+
+  app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+  });
+}
+main()
+  .then(async () => {
+    await prisma.$connect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
 
 
 app.use("/api/post", PostRouter);
