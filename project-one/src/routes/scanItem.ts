0aa34bc974081e64679b748c7007658d9091851d
Taipan <123_marcel@web.de>
2025-12-01 13:10:14 +0100

    added prismaClient dependency

--- project-one/src/routes/scanItem.ts
+++ project-one/src/routes/scanItem.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from 'express';
-import { PrismaClient } from "./generated/prisma/client";
+import { PrismaClient } from '@prisma/client';
 
 const router = express.Router();
 const prisma = new PrismaClient();
