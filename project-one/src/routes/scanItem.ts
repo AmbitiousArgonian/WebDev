5001d93926785dc03d5e03045642f93992cc2c7d
Taipan <123_marcel@web.de>
2025-12-01 14:02:27 +0100

    npm install express essions

--- project-one/src/routes/scanItem.ts
+++ project-one/src/routes/scanItem.ts
@@ -1,6 +1,6 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import type { Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
-
 const router = express.Router();
 const prisma = new PrismaClient();
 
